c97f0bf58e88b794a65802599c9657bc75599f26
jose-guilherme93 <jose-guilherme93@hotmail.com>
2025-09-29 16:24:16 -0300

    feat: add user existence check before registration in createUserController

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,21 @@
 import {pool} from '../database/connectDatabase.js'
 
+export const checkUser = async (username, email) => {
+  const ifUserExists = `SELECT * FROM USERS WHERE username = $1 OR email = $2`
+  const checkResult = await pool.query(ifUserExists,[username, email])
+  return checkResult.rows
+}
+
 export const createUserDB = async (id, username, email, password, avatar ) => {
+
   id = crypto.randomUUID()
-      const query = `
-        INSERT INTO users (id, username, email, password, avatar)
-        VALUES ($1, $2, $3, $4, $5)
-        RETURNING *;
-        `
-        const result = await pool.query(query, [id, username, email, password, avatar])
-        return result.rows[0]
+
+  const query = `INSERT INTO users (id, username, email, password, avatar)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *;
+    `
+    const result = await pool.query(query, [id, username, email, password, avatar])
+    return result.rows[0]
 }
 
 const updateUser = async () => {
--- routes/controllers/userController.js
+++ routes/controllers/userController.js
@@ -1,17 +1,24 @@
 import { logger } from "../../logger.js"
-import { createUserDB } from "../../models/userModel.js"
+import { checkUser, createUserDB } from "../../models/userModel.js"
 
 export const createUserController = async (req, res) => {
 
     const {username, email, password, avatar} = req.body
-    const id = crypto.randomUUID()
-   
-    try {
-        const user = await createUserDB(id, username, email, password, avatar)
-        logger.info(user)
-        res.status(201).json(user)
-    }
-    catch (error) {
-        logger.error(error)
+    const check = await checkUser(username, email)
+
+    if(check.length) {return res.send("usuário já cadastrado")}
+
+    else {
+        
+        try {
+            const id = crypto.randomUUID()
+            
+            const user = await createUserDB(id, username, email, password, avatar)
+            logger.info(user)
+            res.status(201).json(user)
+        }
+        catch (error) {
+            logger.error(error)
+        }
     }
 }
