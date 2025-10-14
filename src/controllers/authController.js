2787e564b3e004875006652c03331999b1a430cd
jose-guilherme93 <jose-guilherme93@hotmail.com>
2025-10-14 13:05:39 -0300

    fix: add validation in newPasswordHash query parameter

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -100,23 +100,30 @@ export const forgotPasswordController = (req, res) => {
 
 export const resetPasswordController = async (req, res) => {
   const { recoveryToken } = req.query
-
+  if(!recoveryToken)
+  res.status(400).json({ message: "Token de recuperação é obrigatório" })
 
   const {newPasswordHash} = req.body
-
-  if(!recoveryToken) {
-   return res.status(400).json({ message: "Token de recuperação é obrigatório" })
-  }
-
+  if(newPasswordHash.length < 6) 
+  return res.status(400).json({ message: "senha precisa ter mais de 6 digitos" })
+ 
   try {
-    const responseDb = await pool.query(`SELECT * FROM recovery WHERE code = $1`, [recoveryToken])
+    const responseDb = await pool.query(`
+      SELECT * 
+      FROM recovery 
+      WHERE code = $1`, [recoveryToken])
     
 
-    if(responseDb.rows.length == 0 || responseDb.rowCount === 0) {
-      return res.status(401).json({message: "código de verificação inválido"})
+  if(responseDb.rows.length == 0 || responseDb.rowCount === 0) {
+    return res.status(401).json({message: "código de verificação inválido"})
 
   } else {
-    const responseDbRecoveryPassword = await pool.query(`UPDATE users SET password_hash = $1 WHERE id = $2 RETURNING *`,[newPasswordHash, responseDb.rows[0].user_id])
+    const responseDbRecoveryPassword =
+    await pool.query(`
+      UPDATE users 
+      SET password_hash = $1 
+      WHERE id = $2 RETURNING *`,[newPasswordHash, responseDb.rows[0].user_id])
+
     return res.status(200).json({message: responseDbRecoveryPassword.rows[0]})
   }
     
