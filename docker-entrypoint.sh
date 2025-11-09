90c5dd8512019a3953827b1e05968e3ac6fabcf9
jose-guilherme93 <jose-guilherme93@hotmail.com>
2025-11-09 10:36:09 -0300

    update workflow

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+
+export PGHOST="$PGHOST"
+export PGPORT="$PGPORT"
+export POSTGRES_USER="$POSTGRES_USER"
+export POSTGRES_PASSWORD="$POSTGRES_PASSWORD"
+export POSTGRES_DB="$POSTGRES_DB"
+
+
 echo "Waiting for postgres at $PGHOST:$PGPORT..."
 while ! nc -z "$PGHOST" "$PGPORT"; do
   sleep 0.5
