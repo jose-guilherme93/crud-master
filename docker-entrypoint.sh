c639f4f84dc9d3480645e11465f5a88d25043f7b
jose-guilherme93 <jose-guilherme93@hotmail.com>
2025-11-09 16:44:41 -0300

    update workflow

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,8 +14,4 @@ while ! nc -z "$PGHOST" "$PGPORT"; do
   sleep 0.5
 done
 
-echo "Postgres is up - executing migrations"
-pnpm run migrate
-
-echo "Migrations completed. Starting application server"
 exec "$@"
